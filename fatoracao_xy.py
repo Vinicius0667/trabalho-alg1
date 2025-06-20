5c4426d3b627c82f9ca55ea18c2d2b004c4837c3
Vini0667 <vini04.silva09+github@gmail.com>
2025-06-19 20:37:14 -0400

    Make the functions to help to calculate lu

--- fatoracao_xy.py
+++ fatoracao_xy.py
@@ -1,2 +1,34 @@
 # -*- coding: utf-8 -*-
 
+# 
+# Verificar quando a divisão é por zero (não sei onde tem divisão nessa porra)
+# Fazer o calculo das matriz upper e lower e retorna-las a main
+# Fazer o calculo na função calculate_lu
+#
+
+def show(matrix, size):
+    for i in range(size):
+        for n in range(size):
+            print(f'{matrix:.2f}', end='\t')
+        print()
+    
+# Terminar o calculo aqui
+def calculate_lu(matriz, size):
+    upper_matriz, lower_matriz = make_matrices(size)
+    
+    return upper_matriz, lower_matriz
+    
+def make_matrices(size: int):
+    return [[0] * size for _ in range(size)], [[1 if i == n else 0 for i in range(size)] for n in range(size)]
+
+def main():
+    size = int(input())    
+    matriz = [list(map(float, input().split())) for _ in range(size)]
+    upper_matrix, lower_matrix = calculate_lu(matriz, size)
+
+    show(upper_matrix, size)
+    show(lower_matrix, size)
+
+    return 0
+    
+main()
