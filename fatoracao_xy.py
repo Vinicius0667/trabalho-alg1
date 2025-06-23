90e6767d56d6620fb5bfea2c22f35040961ba27c
PedroUrbieta <125492544+PedroUrbieta@users.noreply.github.com>
2025-06-22 21:25:43 -0300

    Update fatoracao_xy.py
    função calculate_lu complementada

--- fatoracao_xy.py
+++ fatoracao_xy.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 
 '''
@@ -6,7 +7,7 @@ Após isso, nós testamos e vemos a melhor versão, depois daremos merge
 '''
 
 # 
-# Verificar quando a divisão é por zero (não sei onde tem divisão nessa porra)
+# Verificar quando a divisão é por zero (não sei onde tem divisão nessa porra) P: Nem Eu
 # Fazer o calculo das matriz upper e lower e retorna-las a main
 # Fazer o calculo na função calculate_lu
 #
@@ -20,6 +21,29 @@ def show(matrix, size):
 # Fazer o calculo aqui
 def calculate_lu(matriz, size):
     upper_matriz, lower_matriz = make_matrices(size)
+    
+    
+    
+    for k in range(size):
+        for j in range(k, size):
+            soma = 0
+            for s in range(k):
+                soma += lower_matriz[k][s] * upper_matriz[s][j]
+            upper_matriz[k][j] = matriz[k][j] - soma    
+        for i in range(k+1, size):
+            soma = 0
+            for s in range(k):
+                soma += lower_matriz[i][s] * upper_matriz[s][k]
+                
+            
+            pivo = upper_matriz[k][k]
+            if pivo == 0:
+                raise ZeroDivisionError(f"Divisão por zero no pivô U[{k}][{k}]")
+
+            lower_matriz[i][k] = (matriz[i][k] - soma) / pivo
+
+    
+    
     
     return upper_matriz, lower_matriz
     
